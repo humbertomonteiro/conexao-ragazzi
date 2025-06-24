288230620357b8eb01c84de8010b301a4774ce8c
Humberto Monteiro <humbertoaraujox@icloud.com>
2025-06-24 15:15:12 -0300

    Ajuste no bg das imagens

--- src/components/sections/Support/index.tsx
+++ src/components/sections/Support/index.tsx
@@ -1,10 +1,13 @@
 import CarouselScrollInfinit from "../../shared/CaroselScrollInfinit";
 import styles from "./support.module.css";
 
-import ad from "../../../assets/images/support/AD.jpg";
+// import ad from "../../../assets/images/support/AD.jpg";
+import ad from "../../../assets/images/support/ad.png";
 import bevi from "../../../assets/images/support/bevi.png";
-import gft from "../../../assets/images/support/gft.jpg";
-import nova from "../../../assets/images/support/nova.jpeg";
+// import gft from "../../../assets/images/support/gft.jpg";
+import gft from "../../../assets/images/support/gft-no-bg.png";
+// import nova from "../../../assets/images/support/nova.jpeg";
+import nova from "../../../assets/images/support/nova-no-bg.png";
 
 const listSlides = [
   {
